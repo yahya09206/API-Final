603e88991d62a66dea9a58a64caa66bcbff6c71a
Yahya <yahyahussein91@gmail.com>
2024-05-13 10:33:36 -0700

    had to change to adding only first name because full name was too many chars

--- src/test/java/com/yahya/Day5/PostPutRequestWithPOJO.java
+++ src/test/java/com/yahya/Day5/PostPutRequestWithPOJO.java
@@ -4,15 +4,37 @@ import com.github.javafaker.Faker;
 import com.yahya.POJO.Spartan;
 import org.junit.jupiter.api.Test;
 
-public class PostPutRequestWithPOJO {
+import com.yahya.utility.SpartanTestBase;
+import com.yahya.utility.SpartanUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class PostPutRequestWithPOJO extends SpartanTestBase{
 
 
     @Test
     public void testPostWithPojoAsBody(){
 
         Faker faker = new Faker();
-        Spartan spartan1 = new Spartan(faker.name().fullName(), faker.demographic().sex(), 5555555555L);
+        Spartan spartan1 = new Spartan(faker.name().firstName(), faker.demographic().sex(), 5555555555L);
         System.out.println("spartan1 = " + spartan1);
+
+        given().log().all()
+                .contentType(ContentType.JSON)
+                .body(spartan1)
+                .when().post("/spartans")
+                .then().log().all()
+                .statusCode(201);
     }
 
 }
