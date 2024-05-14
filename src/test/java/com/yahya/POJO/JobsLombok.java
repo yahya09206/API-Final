2725f394f42ab11bd64d3e8371a1ef8fd940ae36
Yahya <yahyahussein91@gmail.com>
2024-05-14 10:39:13 -0700

    reconstructing jobs pojo using lombok to erase use of extra code

---
+++ src/test/java/com/yahya/POJO/JobsLombok.java
@@ -0,0 +1,23 @@
+package com.yahya.POJO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JobsLombok {
+
+    @JsonProperty("job_id")
+    private String jobId;
+    @JsonProperty("job_title")
+    private String jobTitle;
+    @JsonProperty("min_salary")
+    private int minSalary;
+    @JsonProperty("max_salary")
+    private int maxSalary;
+}
