c92a6980ca5efa7ee75ec5ea3c786f719a7f8e3b
Yahya <yahyahussein91@gmail.com>
2024-05-14 10:15:37 -0700

    setting up mock car class with lombok

---
+++ src/test/java/com/yahya/POJO/CarLombok.java
@@ -0,0 +1,18 @@
+package com.yahya.POJO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CarLombok {
+
+    private String model;
+    private String make;
+    private int year;
+    private boolean autopilot;
+
+
+}
