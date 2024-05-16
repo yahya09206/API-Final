5a1dcf9a5e2af52fc27bd3cf7b44de5486618e8a
Yahya <yahyahussein91@gmail.com>
2024-05-16 12:07:24 -0700

    print out request to verify it is working

--- src/test/java/com/yahya/Day8/LibraryLoginTest.java
+++ src/test/java/com/yahya/Day8/LibraryLoginTest.java
@@ -1,5 +1,6 @@
 package com.yahya.Day8;
 
+import com.github.javafaker.Faker;
 import com.yahya.utility.LibraryTestBase;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 
@@ -34,15 +38,26 @@ public class LibraryLoginTest extends LibraryTestBase {
 
         // first get library token by sending POST /login request
         // and save it(eventually will make a method out of it
-        given().log().all()
+        String libraryToken = given().log().all()
                 .contentType(ContentType.URLENC)
                 .formParam("email", "librarian3@library")
                 .formParam("password", "libraryUser")
                 .when().post("/login").path("token");
+        System.out.println("libraryToken = " + libraryToken);
 
+        Faker faker = new Faker();
 
         // if you have many form parameters as the body
         // you can use formParams method and pass map object instead
+        Map<String, Object> bookMap = new LinkedHashMap<>();
+        bookMap.put("name", faker.book().title());
+        bookMap.put("isbn", faker.numerify("######"));
+        bookMap.put("year", faker.number().numberBetween(1000, 2024));
+        bookMap.put("author", faker.book().author());
+        bookMap.put("book_category_id", faker.number().numberBetween(1, 20));
+        bookMap.put("description", faker.chuckNorris().fact());
+
+        System.out.println("bookMap = " + bookMap);
 
         // send request to POST /add_book
     }
