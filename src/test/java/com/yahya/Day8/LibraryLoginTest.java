65529af25adda22883ce3603c8c2682c9d8277f3
Yahya <yahyahussein91@gmail.com>
2024-05-16 11:50:00 -0700

    setting up to login with form params

--- src/test/java/com/yahya/Day8/LibraryLoginTest.java
+++ src/test/java/com/yahya/Day8/LibraryLoginTest.java
@@ -1,6 +1,28 @@
 package com.yahya.Day8;
 
-public class LibraryLoginTest {
+import com.yahya.utility.LibraryTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static io.restassured.RestAssured.*;
+
+public class LibraryLoginTest extends LibraryTestBase {
 
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "/librarycredentials.csv", numLinesToSkip = 1)
+    public void testLogin(String username, String password){
+
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+
+        given().log().all()
+                .contentType(ContentType.URLENC)
+                .formParam("email", username)
+                .formParam("password", password)
+                .when().get("/login")
+                .then().log().ifValidationFails().statusCode(200);
+    }
 }
