46a8d6308b005e6596eb2ea536c2a598e68212f3
Yahya <yahyahussein91@gmail.com>
2024-05-16 11:27:42 -0700

    going into auth

---
+++ src/test/java/com/yahya/Day8/BaseAuthTest.java
@@ -0,0 +1,8 @@
+package com.yahya.Day8;
+
+public class BaseAuthTest {
+
+    // Spartan App that runs on port 7000 requires basic auth to make authorized requests
+    // 3 roles with different authority levels admin/admin editor/editor user/user
+    // In query param or header according to the docs
+}
---
+++ src/test/java/com/yahya/utility/SpartanAuthTestBase.java
@@ -0,0 +1,32 @@
+package com.yahya.utility;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.reset;
+
+/**
+ * This class will serve as Base Class to set up BaseURI and BasePATH
+ * and clean up after all the tests for any Spartan related test classes
+ */
+
+public class SpartanAuthTestBase {
+
+    @BeforeAll
+    public static void setup(){
+
+        RestAssured.baseURI = "http://44.211.192.252:8000";
+        RestAssured.basePath = "/api";
+    }
+
+    @AfterAll
+    public static void tearDown(){
+
+        // In order to avoid the static value accidentally being carried over to different classes when using different
+        // apis, it's better to set the baseURI basePATH back to its original value using reset method
+        // RestAssured.rest()
+        reset();
+
+    }
+}
