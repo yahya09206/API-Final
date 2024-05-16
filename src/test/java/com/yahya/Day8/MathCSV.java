961c1a0eb3b659615d3b57de31e508d262d49214
Yahya <yahyahussein91@gmail.com>
2024-05-16 09:59:56 -0700

    using methodSource to get data from different class to use inside of test

--- src/test/java/com/yahya/Day8/MathCSV.java
+++ src/test/java/com/yahya/Day8/MathCSV.java
@@ -2,6 +2,10 @@ package com.yahya.Day8;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.equalTo;
@@ -18,5 +22,24 @@ public class MathCSV {
 
     /**
      * Another way of providing a source for a @ParameterizedTest is @MethodSource
+     * It allows you to provide the returned value from the static method with no param
+     * as the source for your parameterized test
      */
+    @ParameterizedTest
+    @MethodSource("getTenNumbers")
+    public void testWithMethodSourceDDT(int num){
+        System.out.println("num = " + num);
+    }
+    public static List<Integer> getTenNumbers(){
+
+        // This list could have been the result of some very long code
+        List<Integer> myList = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        return myList;
+    }
+
+    @ParameterizedTest
+    @MethodSource("com.yahya.Day8.MethodSourceUtil#getSomeNames")
+    public void testNamesWithMethodSourceDDT(String name){
+        System.out.println("name = " + name);
+    }
 }
---
+++ src/test/java/com/yahya/Day8/MethodSourceUtil.java
@@ -0,0 +1,18 @@
+package com.yahya.Day8;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The only purpose of this class is to demonstrate the @MethodSource
+ * for a parameterized test and the method exists outside of the test class
+ */
+public class MethodSourceUtil {
+
+    // Create a static method that returns a list of names
+    // So we can use the return value as a data source for our parameterized test
+    public static List<String> getSomeNames(){
+
+        return Arrays.asList("Furkan", "Abbos", "Yevhniia", "Shazia", "Tugba");
+    }
+}
