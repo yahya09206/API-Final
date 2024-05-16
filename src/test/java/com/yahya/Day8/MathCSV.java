64014e9ede2c77a1c75917f2806d72f55fd9e32a
Yahya <yahyahussein91@gmail.com>
2024-05-16 09:45:43 -0700

    asserted with hamcrest matchers

--- src/test/java/com/yahya/Day8/MathCSV.java
+++ src/test/java/com/yahya/Day8/MathCSV.java
@@ -3,6 +3,9 @@ package com.yahya.Day8;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class MathCSV {
 
     @ParameterizedTest
@@ -10,5 +13,10 @@ public class MathCSV {
     public void testMath(int num1, int num2, int expectedResult){
 
         System.out.println(num1 + " " + num2 + " should be " + expectedResult);
+        assertThat(num1 + num2, equalTo(expectedResult));
     }
+
+    /**
+     * Another way of providing a source for a @ParameterizedTest is @MethodSource
+     */
 }
