e269c6a90f694378779f0164456a8d4bc3d91ce0
Yahya <yahyahussein91@gmail.com>
2024-05-17 09:32:13 -0700

    using different options for running in testrunner

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -1,12 +1,16 @@
 package com.yahya.runner;
 
+import com.yahya.Day8.BaseAuthTest;
+import com.yahya.Day8.LibraryLoginTest;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SuiteDisplayName("Smoke Test")
-@SelectPackages("com.yahya.Day8")
+//@SelectPackages("com.yahya.Day8") // if you want to run a whole package
+@SelectClasses(LibraryLoginTest.class) // If you just want to run 1 class
 public class TestRunner {
 
 
