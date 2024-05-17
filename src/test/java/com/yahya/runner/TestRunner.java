84e8fc2808e1b6d9a3e380e8e882afef7b2daca9
Yahya <yahyahussein91@gmail.com>
2024-05-17 09:52:11 -0700

    fixed class naming issue so testrunner could find correct class

--- src/test/java/com/yahya/runner/TestRunner.java
+++ src/test/java/com/yahya/runner/TestRunner.java
@@ -2,16 +2,15 @@ package com.yahya.runner;
 
 import com.yahya.Day8.BaseAuthTest;
 import com.yahya.Day8.LibraryLoginTest;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.platform.suite.api.*;
 
 @Suite
 @SuiteDisplayName("Smoke Test")
 //@SelectPackages("com.yahya.Day8") // if you want to run a whole package
-@SelectPackages({"com.yahya.Day7", "com.yahya.Day6"})
+//@SelectPackages("com.yahya.Day9")
 //@SelectClasses(LibraryLoginTest.class) // If you just want to run 1 class
+@SelectPackages("com.yahya.Day9")
+@IncludeTags("smokeAll")
 public class TestRunner {
 
 
