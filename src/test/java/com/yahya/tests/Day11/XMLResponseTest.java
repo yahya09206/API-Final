ffb9738edc7dd21d8044f96c2f214bedd23a864f
Yahya <yahyahussein91@gmail.com>
2024-05-21 12:19:38 -0700

    testing response for xml

---
+++ src/test/java/com/yahya/tests/Day11/XMLResponseTest.java
@@ -0,0 +1,40 @@
+package com.yahya.tests.Day11;
+
+import com.yahya.utility.SpartanTestBase;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+public class XMLResponseTest {
+
+    @BeforeAll
+    public static void setUp(){
+        baseURI = "http://ergast.com";
+        basePath = "api/f1";
+    }
+
+    @AfterAll
+    public static void tearDown(){
+        reset();
+    }
+
+    @DisplayName("Test GET /drivers/{drivers{id} xml response")
+    @Test
+    public void testSingleDriverXMLResponse(){
+
+        given()
+                .pathParam("driverId", "abate")
+                .log().uri()
+                .when().get("/drivers/{driverId}")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.XML);
+    }
+}
